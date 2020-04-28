a3573de6185fbf0ea356f6f9a1c33c4109b7824e
Alex Telegaru <alextelegaru@users.noreply.github.com>
2020-04-28 10:11:18 +0200

    user ajax good

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //dsd
+    public function index()
+    {
+        $usuarios=User::all();
+
+        return view("usuarios", compact("usuarios"));
+    }
 }
---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,33 @@
+{{ $usuarios }}
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="demo">
+<h2>The XMLHttpRequest Object</h2>
+<button type="button" onclick="loadDoc()">Change Content</button>
+</div>
+
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =this.responseText;
+    }
+  };
+  xhttp.open("GET", "/usuarios", true);
+  xhttp.send();
+}
+
+
+
+
+
+
+</script>
+
+</body>
+</html>
